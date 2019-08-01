d94581751e3e0df0b932f88e0f4836b7d1a2bde9
Gianluigi Oliva <gianluigi.oliva@mail.polimi.it>
2019-08-01 18:24:43 +0200

    fix shader and look at camera

--- Shader/prova.glsl
+++ Shader/prova.glsl
@@ -6,24 +6,45 @@ in vec3 fs_norm;
 in vec2 fs_uv;
 
 uniform sampler2D u_texture;
-uniform float DTexMix;
 uniform vec3 eyePos;
 
-uniform vec4 lightType;
 uniform vec4 ambientType;
 uniform vec4 diffuseType;
 uniform vec4 specularType;
-uniform vec3 LPos;
-uniform vec3 LDir;
-uniform float LConeOut;
-uniform float LConeIn;
-uniform float LDecay;
-uniform float LTarget;
-uniform vec4 lightColor;
+uniform vec4 emissionType;
+
+uniform vec4 L0lightType;
+uniform vec3 L0Pos;
+uniform vec3 L0Dir;
+uniform float L0ConeOut;
+uniform float L0ConeIn;
+uniform float L0Decay;
+uniform float L0Target;
+uniform vec4 L0lightColor;
+
+uniform vec4 L1lightType;
+uniform vec3 L1Pos;
+uniform vec3 L1Dir;
+uniform float L1ConeOut;
+uniform float L1ConeIn;
+uniform float L1Decay;
+uniform float L1Target;
+uniform vec4 L1lightColor;
+
+uniform vec4 L2lightType;
+uniform vec3 L2Pos;
+uniform vec3 L2Dir;
+uniform float L2ConeOut;
+uniform float L2ConeIn;
+uniform float L2Decay;
+uniform float L2Target;
+uniform vec4 L2lightColor;
+
 uniform vec4 ambientLightColor;
 uniform vec4 ambientLightLowColor;
 uniform vec3 ADir;
 uniform vec4 diffuseColor;
+uniform float DTexMix;
 uniform vec4 specularColor;
 uniform float SpecShine;
 uniform float DToonTh;
@@ -33,26 +54,29 @@ uniform vec4 emitColor;
 
 out vec4 color;
 
-void main() {
-	vec4 texcol = texture(u_texture, fs_uv);
-	vec4 diffColor = diffuseColor * (1.0-DTexMix) + texcol * DTexMix;
-	vec4 ambColor = ambientMatColor * (1.0-DTexMix) + texcol * DTexMix;
-	vec4 emit = emitColor * (1.0-DTexMix) +
-				   texcol * DTexMix * 
-				   			max(max(emitColor.r, emitColor.g), emitColor.b);
-//				   		   (0.299*emitColor.r + 0.587*emitColor.g + 0.114*emitColor.b);
-	
-	vec3 normalVec = normalize(fs_norm);
-	vec3 eyedirVec = normalize(eyePos - fs_pos);
-	float LCosOut = cos(radians(LConeOut / 2.0));
-	float LCosIn = cos(radians(LConeOut * LConeIn / 2.0));
-	
+vec3 compLightDir(vec3 LPos, vec3 LDir, vec4 lightType) {
 	//lights
 	// -> Point
 	vec3 pointLightDir = normalize(LPos - fs_pos);
-	vec4 pointLightCol = lightColor * pow(LTarget / length(LPos - fs_pos), LDecay);
 	// -> Direct
 	vec3 directLightDir = LDir;
+	// -> Spot
+	vec3 spotLightDir = normalize(LPos - fs_pos);
+
+	return            directLightDir * lightType.x +
+					  pointLightDir * lightType.y +
+					  spotLightDir * lightType.z;
+}
+
+vec4 compLightColor(vec4 lightColor, float LTarget, float LDecay, vec3 LPos, vec3 LDir,
+					float LConeOut, float LConeIn, vec4 lightType) {
+	float LCosOut = cos(radians(LConeOut / 2.0));
+	float LCosIn = cos(radians(LConeOut * LConeIn / 2.0));
+
+	//lights
+	// -> Point
+	vec4 pointLightCol = lightColor * pow(LTarget / length(LPos - fs_pos), LDecay);
+	// -> Direct
 	vec4 directLightCol = lightColor;
 	// -> Spot
 	vec3 spotLightDir = normalize(LPos - fs_pos);
@@ -60,23 +84,12 @@ void main() {
 	vec4 spotLightCol = lightColor * pow(LTarget / length(LPos - fs_pos), LDecay) *
 						clamp((CosAngle - LCosOut) / (LCosIn - LCosOut), 0.0, 1.0);
 	// ----> Select final component
-	vec3 lightDir = directLightDir * lightType.x +
-					pointLightDir * lightType.y +
-					spotLightDir * lightType.z;
-	vec4 lightCol = directLightCol * lightType.x +
+	return          directLightCol * lightType.x +
 					pointLightCol * lightType.y +
 					spotLightCol * lightType.z;
-	
-	// Ambient
-	// --> Ambient
-	vec4 ambientAmbient = ambientLightColor * ambColor;
-	// --> Hemispheric
-	float amBlend = (dot(normalVec, ADir) + 1.0) / 2.0;
-	vec4 ambientHemi = (ambientLightColor * amBlend + ambientLightLowColor * (1.0 - amBlend)) * ambColor;
-	// ----> Select final component
-	vec4 ambient = ambientAmbient * ambientType.x +
-				   ambientHemi * ambientType.y;
-	
+}
+
+vec4 compDiffuse(vec3 lightDir, vec4 lightCol, vec3 normalVec, vec4 diffColor) {
 	// Diffuse
 	// --> Lambert
 	vec4 diffuseLambert = lightCol * clamp(dot(normalVec, lightDir),0.0,1.0) * diffColor;
@@ -89,9 +102,11 @@ void main() {
 	}
 	vec4 diffuseToon = lightCol * ToonCol;
 	// ----> Select final component
-	vec4 diffuse = diffuseLambert * diffuseType.x +
+	return         diffuseLambert * diffuseType.x +
 				   diffuseToon * diffuseType.y;
-	
+}
+
+vec4 compSpecular(vec3 lightDir, vec4 lightCol, vec3 normalVec, vec3 eyedirVec) {
 	// Specular
 	// --> Phong
 	vec3 reflection = -reflect(lightDir, normalVec);
@@ -116,10 +131,59 @@ void main() {
 	}
 	vec4 specularToonB = lightCol * ToonSpecBCol;
 	// ----> Select final component
-	vec4 specular = specularPhong * specularType.x +
+	return          specularPhong * specularType.x +
 					specularBlinn * specularType.y +
 					specularToonP * specularType.z +
 					specularToonB * specularType.w;
+}
+
+void main() {
+	vec4 texcol = texture(u_texture, fs_uv);
+	vec4 diffColor = diffuseColor * (1.0-DTexMix) + texcol * DTexMix;
+	vec4 ambColor = ambientMatColor * (1.0-DTexMix) + texcol * DTexMix;
+	vec4 emit = (emitColor * (1.0-DTexMix) +
+				    texcol * DTexMix * 
+				   			max(max(emitColor.r, emitColor.g), emitColor.b)) * emissionType.r;
+//				   		   (0.299*emitColor.r + 0.587*emitColor.g + 0.114*emitColor.b);
+	
+	vec3 normalVec = -normalize(fs_norm);
+	vec3 eyedirVec = normalize(eyePos - fs_pos);
+	
+	//lights
+	vec3 L0lightDir = compLightDir(L0Pos, L0Dir, L0lightType);
+	vec4 L0lightCol = compLightColor(L0lightColor, L0Target, L0Decay, L0Pos, L0Dir,
+								     L0ConeOut, L0ConeIn, L0lightType);
+	
+	vec3 L1lightDir = compLightDir(L1Pos, L1Dir, L1lightType);
+	vec4 L1lightCol = compLightColor(L1lightColor, L1Target, L1Decay, L1Pos, L1Dir,
+								     L1ConeOut, L1ConeIn, L1lightType);
+	
+	vec3 L2lightDir = compLightDir(L2Pos, L2Dir, L2lightType);
+	vec4 L2lightCol = compLightColor(L2lightColor, L2Target, L2Decay, L2Pos, L2Dir,
+								     L2ConeOut, L2ConeIn, L2lightType);
+	
+	// Ambient
+	// --> Ambient
+	vec4 ambientAmbient = ambientLightColor * ambColor;
+	// --> Hemispheric
+	float amBlend = (dot(normalVec, ADir) + 1.0) / 2.0;
+	vec4 ambientHemi = (ambientLightColor * amBlend + ambientLightLowColor * (1.0 - amBlend)) * ambColor;
+	// ----> Select final component
+	vec4 ambient = ambientAmbient * ambientType.x +
+				   ambientHemi * ambientType.y;
+	
+	// Diffuse
+	vec4 diffuse = compDiffuse(L0lightDir, L0lightCol, normalVec, diffColor) + 
+				   compDiffuse(L1lightDir, L1lightCol, normalVec, diffColor) +
+				   compDiffuse(L2lightDir, L2lightCol, normalVec, diffColor);
+	
+	// Specular
+	// --> Phong
+	vec4 specular = compSpecular(L0lightDir, L0lightCol, normalVec, eyedirVec) +
+					compSpecular(L1lightDir, L1lightCol, normalVec, eyedirVec) +
+					compSpecular(L2lightDir, L2lightCol, normalVec, eyedirVec);
+	
+	
 	// final steps
 	vec4 out_color = clamp(ambient + diffuse + specular + emit, 0.0, 1.0);
 	
--- Shader/vsSimple.glsl
+++ Shader/vsSimple.glsl
@@ -6,6 +6,7 @@ in vec2 in_uv;
 
 uniform mat4 pMatrix;
 uniform mat4 wMatrix;
+uniform mat3 nMatrix;
 
 out vec3 fs_pos;
 out vec3 fs_norm;
@@ -13,7 +14,7 @@ out vec2 fs_uv;
 
 void main() {
 	fs_pos = (wMatrix * vec4(in_pos, 1.0)).xyz;
-	fs_norm = in_norm;
+	fs_norm = nMatrix * in_norm;
 	fs_uv = vec2(in_uv.x, 1.0-in_uv.y);
 	
 	gl_Position = pMatrix * vec4(in_pos, 1.0);
